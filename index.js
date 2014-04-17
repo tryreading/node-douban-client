9a28161f62b520142a89519c5914b14fadf0daca
Lei Pan <superpan@gmail.com>
2014-04-17 09:42:14 -0700

    add more apis for books, refactor client loader, preparing to add some tests

--- index.js
+++ index.js
@@ -3,17 +3,43 @@
 var _ = require('underscore');
 var request = require('request');
 var apis = require('./lib');
+
 var apiUrl = "https://api.douban.com/v2";
 
 function Douban (opts) {
+  if (opts.version && opts.version !== 'v2') {
+    throw new Error('only v2 is supported');
+  }
   this.opts = opts;
   return this;
 }
 
 Douban.prototype.get = function (opts, callback) {
   opts = _.defaults(opts, this.opts);
+  
+  // set request url
   var url = apiUrl + opts.path;
-  request.get(url, callback);
+
+  // set default qs and copy over all of the query parameters
+  var qs = {
+    apikey: opts.apikey
+  };
+  _.extend(qs, opts.qs);
+   
+  // set up request
+  request.get(
+    {
+      url: url,
+      qs: qs,
+      json: true
+    },
+    function (err, res) {
+      if (err) {
+        return callback(err);
+      }
+      return callback(null, res.body);
+    }
+  );
 };
 
 Douban.prototype.post = function (opts, callback) {
@@ -22,7 +48,8 @@ Douban.prototype.post = function (opts, callback) {
 };
 
 _.each(apis, function(api, key) {
-  _.extend(api, Douban.prototype);
+  api._client = {};
+  _.extend(api._client, Douban.prototype);
   Douban.prototype[key] = api;
 });
 
